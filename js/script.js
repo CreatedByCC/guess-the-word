228497f1f8e890f1efd381aa60409b60768f5d3d
CC <chantelh88@gmail.com>
2023-06-18 16:31:25 +1200

    Added code to display guessed letters

--- js/script.js
+++ js/script.js
@@ -60,5 +60,40 @@ const makeGuess = function(guess) {
         message.innerText = "You alread guessed that letter. Try again!"
     } else {
         guessedLetters.push(guess);
+        showGuessedLetters();
+        updateUnknownWord(guessedLetters);
+    }
+};
+
+const showGuessedLetters = function() {
+    listOfLetters.innerText = "";
+    for (let letter of guessedLetters) {
+        let li = document.createElement("li");
+        li.innerText = letter;
+        listOfLetters.append(li);
+    }
+};
+
+// replace the dots with the guessed letters
+const updateUnknownWord = function(guessedLetters) {
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    const revealWord = [];
+    for (let letter of wordArray) {
+        if (guessedLetters.includes(letter)) {
+            revealWord.push(letter.toUpperCase());
+        } else {
+            revealWord.push("●");
+        }
+    }
+    unknownWord.innerText = revealWord.join("");
+    winOrLoose();
+};
+
+// check to see if the user won
+const winOrLoose = function() {
+    if (word.toUpperCase() === unknownWord.innerText) {
+        message.classList.add("win");
+        message.innerHTML = `<p class="highlight">You guessed the correct word! Congrats!</p>`;
     }
 };
